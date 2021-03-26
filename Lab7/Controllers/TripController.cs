6154602318f081aa48778f0289f86f166dac62fa
John Michael De Guzman <jmxdg1029@gmail.com>
2021-03-26 18:00:39 -0400

    part 3

--- Lab7/Controllers/TripController.cs
+++ Lab7/Controllers/TripController.cs
@@ -19,7 +19,40 @@ namespace Lab7.Controllers
         [HttpGet]
         public ViewResult Add(string id = "")
         {
+            var vm = new TripViewModel();
+            if(id == "Page2")
+            {
+                var accomodation = TempData[nameof(Trip.Accomidation)].ToString();
+                if(string.IsNullOrEmpty(accomodation))
+                {
+                    vm.PageNumber = 3;
+                    var destination = TempData[nameof(Trip.Destination)].ToString();
+                    vm.Trip = new Trip { Destination = destination };
+                    return View("Add3", vm);
+                }
+                else
+                {
+                    vm.PageNumber = 2;
+                    vm.Trip = new Trip { Accomidation = accomodation };
+                    TempData.Keep(nameof(Trip.Accomidation));
+                    return View("Add2", vm);
+                }
 
+            }else if(id == "Page3")
+            {
+                vm.PageNumber = 3;
+                vm.Trip = new Trip { Destination = TempData.Peek(nameof(Trip.Destination)).ToString() };
+                TempData.Keep(nameof(Trip.Accomidation));
+                return View("Add3", vm);
+
+            }
+            else
+            {
+
+                vm.PageNumber = 1;
+                return View("Add3", vm);
+
+            }
         }
         [HttpPost]
         public IActionResult Add(TripViewModel vm)
@@ -49,9 +82,19 @@ namespace Lab7.Controllers
             {
                 vm.Trip.Destination = TempData[nameof(Trip.Destination)].ToString();
                 vm.Trip.Accomidation = TempData[nameof(Trip.Accomidation)].ToString();
+                vm.Trip.startDate = (DateTime)TempData[nameof(Trip.startDate)];
+                vm.Trip.endDate = (DateTime)TempData[nameof(Trip.endDate)];
+                vm.Trip.AccommodationPhone = TempData[nameof(Trip.AccommodationPhone)].ToString();
+                vm.Trip.AccommodationEmail = TempData[nameof(Trip.AccommodationEmail)].ToString();
+
+                context.Trips.Add(vm.Trip);
+                context.SaveChanges();
+                TempData["message"] = $"Trip to {vm.Trip.Destination} added";
+                return RedirectToAction("Index","Home");
             }
             else
             {
+                return RedirectToAction("Index", "Home");
 
             }
         }
